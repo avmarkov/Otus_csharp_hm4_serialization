4e58cbd799de189af76d450706dec92032459209
MarkovAV <a.markov@mirtekgroup.ru>
2023-04-04 14:33:53 +0300

    Добавьте файлы проекта.

---
+++ MySerializer.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace hm3_serialization
+{
+    public static class MySerializer<T>
+    {
+        public static string Serialize(T obj)
+        {
+            var sb = new StringBuilder();
+            var myType = obj.GetType();
+            var fields = myType.GetFields();
+
+            foreach (var field in fields)
+            {
+                if (sb.Length > 0)
+                {
+                    sb.Append(";");
+                }                    
+                sb.Append(field.Name);
+                sb.Append("=");
+                sb.Append(field.GetValue(obj).ToString());
+                
+            }
+            return sb.ToString();
+        }
+
+        public static T DeSerialize(string s)
+        {
+            var myType = typeof(T);
+            var instance = Activator.CreateInstance(myType);
+
+            var strfieldValues = s.Split(';');
+            foreach (var strfieldValue in strfieldValues)
+            {
+                var oneStrField = strfieldValue.Split('=');
+                var field = myType.GetField(oneStrField[0]);
+                field.SetValue(instance, Convert.ChangeType(oneStrField[1], field.FieldType));
+            }
+
+            return (T)instance;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿
+using hm3_serialization;
+using Newtonsoft.Json;
+
+F f = new F().Get();
+int count = 1000000;
+string objStr = "";
+
+// Моя сериализация
+var startTime = System.Diagnostics.Stopwatch.StartNew();
+for (int i = 0; i < count; i++)
+{
+    objStr = MySerializer<F>.Serialize(f);
+}
+
+startTime.Stop();
+Console.WriteLine($"My serialization time: {startTime.Elapsed}");
+Console.WriteLine($"My searilizated string: {objStr}");
+
+// Моя десериализация
+startTime = System.Diagnostics.Stopwatch.StartNew();
+for (int i = 0; i < count; i++)
+{
+    F deserializedF = MySerializer<F>.DeSerialize(objStr);
+}
+startTime.Stop();
+Console.WriteLine($"My deserialization time: {startTime.Elapsed}");
+Console.WriteLine();
+
+
+// Newtonsoft.Json сериализация
+string jsonStr = "";
+startTime = System.Diagnostics.Stopwatch.StartNew();
+for (int i = 0; i < count; i++)
+{
+    jsonStr = JsonConvert.SerializeObject(f);
+}
+startTime.Stop();
+Console.WriteLine($"Newtonsoft.Json serialization time: {startTime.Elapsed}");
+
+
+// Newtonsoft.Json десериализация
+startTime = System.Diagnostics.Stopwatch.StartNew();
+for (int i = 0; i < count; i++)
+{
+    F deserializedJsonF = JsonConvert.DeserializeObject<F>(jsonStr);
+}
+startTime.Stop();
+Console.WriteLine($"Newtonsoft.Json deserialization time: {startTime.Elapsed}");
+
+
+
